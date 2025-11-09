9cbeab3a6e8a73c4c8fb73532543b68fd5dcba80
tusharn3115 <negitushar81@gmail.com>
2025-11-09 16:27:06 +0530

    installed gsap for some animations

--- src/components/home/HomeBtn.jsx
+++ src/components/home/HomeBtn.jsx
@@ -4,10 +4,10 @@ import { Link } from "react-router-dom";
 const HomeBtn = () => {
   return (
     <div className="font-[lausBold] flex items-center justify-center gap-2">
-      <Link to="/projects" className="text-[5.5vw] leading-[6vw] border-3 border-white rounded-full uppercase px-10 pt-3 hover:border-[#D3FD50] hover:text-[#D3FD50] transition-all duration-200">
+      <Link to="/projects" className="text-[6vw] leading-[6vw] border-3 border-white rounded-full uppercase px-7 pt-3 hover:border-[#D3FD50] hover:text-[#D3FD50] transition-all duration-200">
         Projects
       </Link>
-      <Link to="/agence" className="text-[5.5vw] leading-[6vw] border-3 border-white rounded-full uppercase px-10 pt-3 hover:border-[#D3FD50] hover:text-[#D3FD50] transition-all duration-200">
+      <Link to="/agence" className="text-[6vw] leading-[6vw] border-3 border-white rounded-full uppercase px-7 pt-3 hover:border-[#D3FD50] hover:text-[#D3FD50] transition-all duration-200">
         Agence
       </Link>
     </div>
--- src/pages/Agence.jsx
+++ src/pages/Agence.jsx
@@ -1,19 +1,60 @@
-import React from "react";
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/all";
+import React, { useRef } from "react";
 
 const Agence = () => {
+
+  const imageDivRef = useRef(null);
+
+  gsap.registerPlugin(ScrollTrigger);
+
+  useGSAP(function(){
+    gsap.to(imageDivRef.current, {
+      scrollTrigger: {
+        trigger: imageDivRef.current,
+        markers: true,
+        start: 'top 27.5%',
+        end: "top -100%",
+        pin: true,
+        scrub: true,
+      }
+    })
+  })
+
   return (
-    <div className="font-[lausBold]">
-      <div className="mt-[55vh]">
-        <h1 className="text-[19vw] text-center uppercase leading-[17vw]">
-          Soixan7e <br />
-          Douze
-        </h1>
+    <div>
+      <div className="section1">
+        <div ref={imageDivRef} className="absolute overflow-hidden h-[20vw] w-[15vw] rounded-3xl top-50 left-[30vw] border border-zinc-400/20">
+          <img
+            className="h-full w-full object-cover"
+            src="https://k72.ca/images/teamMembers/Carl_480x640.jpg?w=480&h=640&fit=crop&s=f0a84706bc91a6f505e8ad35f520f0b7"
+            alt="img"
+          />
+        </div>
+        <div className="relative font-[lausBold]">
+          <div className="mt-[55vh]">
+            <h1 className="text-[19vw] text-center uppercase leading-[17vw]">
+              Soixan7e <br />
+              Douze
+            </h1>
+          </div>
+
+          <div className="pl-[40%] mt-16">
+            <p className="text-5xl">
+              &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+              Notre curiosité nourrit notre créativité. On reste humbles et on
+              dit non aux gros egos, même le vôtre. Une marque est vivante. Elle
+              a des valeurs, une personnalité, une histoire. Si on oublie ça, on
+              peut faire de bons chiffres à court terme, mais on la tue à long
+              terme. C’est pour ça qu’on s’engage à donner de la perspective,
+              pour bâtir des marques influentes.
+            </p>
+          </div>
+        </div>
       </div>
 
-      <div className="pl-[40%] mt-16">
-        <p className="text-5xl">
-          &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; Notre curiosité nourrit notre créativité. On reste humbles et on dit non aux gros egos, même le vôtre. Une marque est vivante. Elle a des valeurs, une personnalité, une histoire. Si on oublie ça, on peut faire de bons chiffres à court terme, mais on la tue à long terme. C’est pour ça qu’on s’engage à donner de la perspective, pour bâtir des marques influentes.
-        </p>
+      <div className="section2 h-screen">
       </div>
     </div>
   );
